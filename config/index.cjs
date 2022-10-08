3c6b977b86e1f8b75b17f3060ac15e550523d0e0
ZYJandMWH <115339265+ZYJandMWH@users.noreply.github.com>
2022-10-08 17:26:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '张艳洁', year: '2001', date: '03-02',
+          type: '生日', name: '张艳洁', date: '03-02',
         },
         {
           type: '节日', name: '相识纪念日', year: '2022', date: '06-08',
