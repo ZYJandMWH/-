5486d3a0c92496978b7d0a2d6dd3293696ec94c7
ZYJandMWH <115339265+ZYJandMWH@users.noreply.github.com>
2022-10-08 17:09:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -34,11 +34,10 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-06-08' },
-        
+        { keyword: 'love_day', date: '2022-06-08' }, 
       ],
     },
-    }
+    
   ],
 
 
