7ee37f88307344098ed5b9a80d6c8799b78c5f36
ZYJandMWH <115339265+ZYJandMWH@users.noreply.github.com>
2022-10-08 17:14:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oJUOO6WX1oNTgWpijP9Ur8dyv_PM',id: 'oJUOO6aPPBCBdwilyzC71QgdmT98',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'B37PPbj8xNRSLGGCc9kowHaxOxf6L-GU7T-YhD7dtoU',
+      useTemplateId: '-GqRrWkeHrrZPYUc48G8eK790C_OiS0RRGlaCsngyQ0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-02',
       festivals: [
