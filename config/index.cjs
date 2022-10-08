d72534616f88cca708bd62bcae184faf28f2d64f
ZYJandMWH <115339265+ZYJandMWH@users.noreply.github.com>
2022-10-08 17:57:53 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -38,12 +38,36 @@ const USER_CONFIG = {
         
       ],
     },
-    
+    {
+      // 想要发送的人的名字
+      name: '自己',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'oJUOO6aPPBCBdwilyzC71QgdmT98',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 't_he9HRWkdwjA2B4vNoY8F5lgmPgctacSzwvKwcgXMg',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '03-02',
+      festivals: [
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', date: '03-02',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2022', date: '06-08',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2022-06-08' }, 
+        
+      ],
+    },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'iqVC45PqlC6aaNW9rtcg_M370B0cWzCKCRcIQmUn0CY',
+  
 
   CALLBACK_USERS: [
     {
